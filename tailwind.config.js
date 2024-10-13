80299445bd80a97c21986d51cb78656c2c2374aa
marshall405 <marshall.slemp@gmail.com>
2024-10-13 00:16:53 -0500

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./about/*.{html,js}", "./contact/*.{html,js}", "./menu/*.{html,js}"],
   theme: {
     extend: {},
   },
